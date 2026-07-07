bb356bbcecdd05fed5085938c738749174c06ba1
kylezhao101 <kylezhao101@gmail.com>
2026-07-07 15:34:09 -0700

    Fix packaged Electron entry path again

--- desktop/src/electron/main.ts
+++ desktop/src/electron/main.ts
@@ -131,7 +131,7 @@ function createWindow() {
   if (isDev) {
     win.loadURL("http://localhost:5173");
   } else {
-    win.loadFile(path.join(__dirname, "../dist/index.html"));
+    win.loadFile(path.join(__dirname, "../../dist/index.html"));
   }
 
   win.webContents.on("did-finish-load", () => {
